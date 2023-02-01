fe663b3be926734dbe86a58f2027a3cc0eb3d379
James Kaplan <jimkap09@gmail.com>
2023-02-01 17:19:27 -0500

    Initial commit

---
+++ StataRibbon/Ribbon1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace StataRibbon
+{
+    partial class Ribbon1 : Microsoft.Office.Tools.Ribbon.RibbonBase
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public Ribbon1()
+            : base(Globals.Factory.GetRibbonFactory())
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tab1 = this.Factory.CreateRibbonTab();
+            this.group1 = this.Factory.CreateRibbonGroup();
+            this.editBox1 = this.Factory.CreateRibbonEditBox();
+            this.editBox2 = this.Factory.CreateRibbonEditBox();
+            this.makeDoFile = this.Factory.CreateRibbonButton();
+            this.browseButton = this.Factory.CreateRibbonButton();
+            this.separator1 = this.Factory.CreateRibbonSeparator();
+            this.editConfiguration = this.Factory.CreateRibbonButton();
+            this.group2 = this.Factory.CreateRibbonGroup();
+            this.runStataLinesButton = this.Factory.CreateRibbonButton();
+            this.runStata = this.Factory.CreateRibbonButton();
+            this.group3 = this.Factory.CreateRibbonGroup();
+            this.label1 = this.Factory.CreateRibbonLabel();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.tab1.SuspendLayout();
+            this.group1.SuspendLayout();
+            this.group2.SuspendLayout();
+            this.group3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tab1
+            // 
+            this.tab1.Groups.Add(this.group1);
+            this.tab1.Groups.Add(this.group2);
+            this.tab1.Groups.Add(this.group3);
+            this.tab1.Label = "Stata";
+            this.tab1.Name = "tab1";
+            // 
+            // group1
+            // 
+            this.group1.Items.Add(this.editBox1);
+            this.group1.Items.Add(this.editBox2);
+            this.group1.Items.Add(this.makeDoFile);
+            this.group1.Items.Add(this.browseButton);
+            this.group1.Items.Add(this.separator1);
+            this.group1.Items.Add(this.editConfiguration);
+            this.group1.Label = "Do File";
+            this.group1.Name = "group1";
+            // 
+            // editBox1
+            // 
+            this.editBox1.Label = ".do Folder Path";
+            this.editBox1.Name = "editBox1";
+            this.editBox1.SizeString = "WWWWWWWWWWWWW";
+            this.editBox1.Text = null;
+            // 
+            // editBox2
+            // 
+            this.editBox2.Label = ".do File Name";
+            this.editBox2.Name = "editBox2";
+            this.editBox2.SizeString = "WWWWWWWWWWWWW";
+            this.editBox2.Text = null;
+            // 
+            // makeDoFile
+            // 
+            this.makeDoFile.Label = "Make .do File";
+            this.makeDoFile.Name = "makeDoFile";
+            this.makeDoFile.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.makeDoFile_Click);
+            // 
+            // browseButton
+            // 
+            this.browseButton.Label = "Browse";
+            this.browseButton.Name = "browseButton";
+            this.browseButton.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.browseButton_Click);
+            // 
+            // separator1
+            // 
+            this.separator1.Name = "separator1";
+            // 
+            // editConfiguration
+            // 
+            this.editConfiguration.Label = "Edit Configuration";
+            this.editConfiguration.Name = "editConfiguration";
+            this.editConfiguration.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.editConfiguration_Click);
+            // 
+            // group2
+            // 
+            this.group2.Items.Add(this.runStataLinesButton);
+            this.group2.Items.Add(this.runStata);
+            this.group2.Label = "Stata";
+            this.group2.Name = "group2";
+            // 
+            // runStataLinesButton
+            // 
+            this.runStataLinesButton.Label = "Run Stata Lines";
+            this.runStataLinesButton.Name = "runStataLinesButton";
+            this.runStataLinesButton.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.runStataLinesButton_Click);
+            // 
+            // runStata
+            // 
+            this.runStata.Label = "Run Stata";
+            this.runStata.Name = "runStata";
+            this.runStata.SuperTip = "Runs Column C starting from row 2";
+            this.runStata.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.runStata_Click);
+            // 
+            // group3
+            // 
+            this.group3.Items.Add(this.label1);
+            this.group3.Label = "Error Messages";
+            this.group3.Name = "group3";
+            // 
+            // label1
+            // 
+            this.label1.Label = "Errors: ";
+            this.label1.Name = "label1";
+            // 
+            // Ribbon1
+            // 
+            this.Name = "Ribbon1";
+            this.RibbonType = "Microsoft.Excel.Workbook";
+            this.Tabs.Add(this.tab1);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.Ribbon1_Load);
+            this.tab1.ResumeLayout(false);
+            this.tab1.PerformLayout();
+            this.group1.ResumeLayout(false);
+            this.group1.PerformLayout();
+            this.group2.ResumeLayout(false);
+            this.group2.PerformLayout();
+            this.group3.ResumeLayout(false);
+            this.group3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab tab1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup group1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton editConfiguration;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton browseButton;
+        internal Microsoft.Office.Tools.Ribbon.RibbonSeparator separator1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup group2;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton runStataLinesButton;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton runStata;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton makeDoFile;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonEditBox editBox2;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup group3;
+        protected internal Microsoft.Office.Tools.Ribbon.RibbonEditBox editBox1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonLabel label1;
+    }
+
+    partial class ThisRibbonCollection
+    {
+        internal Ribbon1 Ribbon1
+        {
+            get { return this.GetRibbon<Ribbon1>(); }
+        }
+    }
+}
---
+++ StataRibbon/Ribbon1.cs
@@ -0,0 +1,278 @@
+﻿using Microsoft.Office.Interop.Excel;
+using Microsoft.Office.Tools.Excel;
+using Microsoft.Office.Tools.Ribbon;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+using Worksheet = Microsoft.Office.Interop.Excel.Worksheet;
+using Range = Microsoft.Office.Interop.Excel.Range;
+
+namespace StataRibbon
+{
+    public partial class Ribbon1
+    {
+        private string fileLoc = @"C:\WBG\runDoLines";
+        private void Ribbon1_Load(object sender, RibbonUIEventArgs e)
+        {
+            label1.Label = "";
+
+        }
+
+        private void editConfiguration_Click(object sender, RibbonControlEventArgs e)
+        {
+            Process.Start("notepad.exe", fileLoc + @"\rundo51\rundo.ini");
+        }
+
+        private void runStataLinesButton_Click(object sender, RibbonControlEventArgs e)
+        {
+            Range selection = Globals.ThisAddIn.Application.Selection as Range;
+
+            int rows = selection.Rows.Count;
+            int columns = selection.Columns.Count;
+            if(columns > 1)
+            {
+                label1.Label = "Error: Multiple columns selected";
+                return;
+            }
+            label1.Label = "";
+
+            string output = "";
+            int emptyCellCounter = 0;
+            for (int rowIndex = 1; rowIndex <= rows; ++rowIndex)
+            {
+                Range cell = selection.get_Item(rowIndex, 1) as Range;
+
+                if(cell.Value2 != null)
+                {
+                    output += cell.Value2+"\n";
+                    emptyCellCounter = 0;
+                } else
+                {
+                    output += "\n";
+                    emptyCellCounter++;
+                    if(emptyCellCounter > 4) //stop running after finding 5 empty rows
+                    {
+                        break;
+                    }
+                }
+            }
+
+            runStataCode(output);
+        }
+
+        private void browseButton_Click(object sender, RibbonControlEventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                editBox1.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+
+        private void runStata_Click(object sender, RibbonControlEventArgs e)
+        {
+            var sheet = Globals.ThisAddIn.Application.Worksheets.Cast<Worksheet>()
+                                   .SingleOrDefault(w => w.Name == "Code");
+
+            if(sheet == null)
+            {
+                label1.Label = "Error: No sheet named 'Code'";
+                return;
+            }
+
+            string output = "";
+            int emptyCellCounter = 0;
+            int rowCount = 0;
+            for(int i = 2; i < sheet.Rows.Count; i++)
+            {
+                rowCount++;
+                string value = (string)(sheet.Cells[i, 3] as Range).Value;
+                output += value + "\n";
+                if(String.IsNullOrEmpty(value))
+                {
+                    emptyCellCounter++;
+                }
+                else
+                {
+                    emptyCellCounter = 0;
+                }
+                if(emptyCellCounter > 4)
+                {
+                    break;
+                }
+            }
+            label1.Label = "Processed " + rowCount + " rows";
+
+            runStataCode(output);
+        }
+
+        private void runStataCode(string text)
+        {
+            File.WriteAllText(fileLoc + @"\temp\temp.txt", text);
+
+            // Use ProcessStartInfo class
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.CreateNoWindow = false;
+            startInfo.UseShellExecute = false;
+            startInfo.FileName = fileLoc + @"\rundo51\rundo.exe";
+            startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            startInfo.Arguments = "\""+ fileLoc + "\\temp\\temp.txt\"";
+
+            try
+            {
+                // Start the process with the info we specified.
+                // Call WaitForExit and then the using statement will close.
+                using(Process exeProcess = Process.Start(startInfo))
+                {
+                    exeProcess.WaitForExit();
+                }
+            }
+            catch(Exception e)
+            {
+                // Log error.
+                label1.Label = "Error: " + e.Message;
+            }
+        }
+        private void makeDoFile_Click(object sender, RibbonControlEventArgs e)
+        {
+            string folderPath = editBox1.Text;
+            string fileName = editBox2.Text;
+
+            if(folderPath == null || (folderPath is string && String.IsNullOrEmpty(folderPath)))
+            {
+                label1.Label = "Error: Folder Path is blank!";
+                return;
+            }
+            if(fileName == null || (fileName is string && String.IsNullOrEmpty(fileName)))
+            {
+                label1.Label = "Error: File Name is blank!";
+                return;
+            }
+
+            var headerSheet = Globals.ThisAddIn.Application.Worksheets.Cast<Worksheet>().SingleOrDefault(w => w.Name == "Header");
+            if(headerSheet == null)
+            {
+                label1.Label = "Error: No sheet named 'Header'";
+                return;
+            }
+            var codeSheet = Globals.ThisAddIn.Application.Worksheets.Cast<Worksheet>().SingleOrDefault(w => w.Name == "Code");
+            if(codeSheet == null)
+            {
+                label1.Label = "Error: No sheet named 'Code'";
+                return;
+            }
+
+            string output = "/*****************************************************************************************************\n******************************************************************************************************\n**                                                                                                  **\n**                       INTERNATIONAL INCOME DISTRIBUTION DATABASE (I2D2)                          **\n**                                                                                                  **\n";
+            int emptyCellCounter = 0;
+            for(int i = 1; i < headerSheet.Rows.Count; i++)
+            {
+                var title = (headerSheet.Cells[i, 1] as Range).Value;
+                var info = (headerSheet.Cells[i, 2] as Range).Value;
+                var tag = (headerSheet.Cells[i, 3] as Range).Value;
+                if((title == null || (title is string && String.IsNullOrEmpty(title))) &&
+                    (tag == null || (tag is string && String.IsNullOrEmpty(tag))))
+                {
+                    emptyCellCounter++;
+                    if(emptyCellCounter > 4)
+                    {
+                        break;
+                    }
+                }
+                else
+                {
+                    output += "*" + title + "\t\t\t" + info + "\t\t\t"+tag+"\n";
+                    emptyCellCounter = 0;
+                }
+            }
+            output += "**                                                                                                  **\n******************************************************************************************************\n*****************************************************************************************************/\n";
+
+            emptyCellCounter = 0;
+            int rowCount = 0;
+            for(int i = 2; i < codeSheet.Rows.Count; i++)
+            {
+                rowCount++;
+                var block = (codeSheet.Cells[i, 1] as Range).Value;
+                var variableLongName = (codeSheet.Cells[i, 2] as Range).Value;
+                var stataCode = (codeSheet.Cells[i, 3] as Range).Value;
+                var altCode = (codeSheet.Cells[i, 4] as Range).Value;
+                var largeComment = (codeSheet.Cells[i, 5] as Range).Value;
+                var codeExplanation = (codeSheet.Cells[i, 6] as Range).Value;
+                var originalQuestion = (codeSheet.Cells[i, 7] as Range).Value;
+                var document = (codeSheet.Cells[i, 8] as Range).Value;
+
+
+                if(stataCode == null || (stataCode is string && String.IsNullOrEmpty(stataCode)))
+                {
+                    emptyCellCounter++;
+                    output += "\n";
+                    if(emptyCellCounter > 4)
+                    {
+                        break;
+                    }
+                }
+                else
+                {
+                    emptyCellCounter = 0;
+                }
+
+                if(block is string && !String.IsNullOrEmpty(block))
+                {
+                    output += "\n/*****************************************************************************************************\n*                                                                                                    *\n                                   ";
+                    output += block;
+                    output += "\n*                                                                                                    *\n*****************************************************************************************************/\n\n\n";
+                }
+                if(variableLongName is string && !String.IsNullOrEmpty(variableLongName))
+                {
+                    output += "\n** " + variableLongName + "\n";
+                }
+                if(largeComment is string && !String.IsNullOrEmpty(largeComment))
+                {
+                    output += "\n/*\n" + largeComment + "\n*/\n";
+                }
+                if(originalQuestion is string && !String.IsNullOrEmpty(originalQuestion))
+                {
+                    output += "\n/* Original Question\n" + originalQuestion + "\n*/\n";
+                }
+                if(stataCode is string && !String.IsNullOrEmpty(stataCode))
+                {
+                    output += "\t" + stataCode;
+                    if(codeExplanation is string && !String.IsNullOrEmpty(codeExplanation))
+                    {
+                        output += " // " + codeExplanation;
+                    }
+                    output += "\n";
+                }
+                else
+                {
+                    if(codeExplanation is string && !String.IsNullOrEmpty(codeExplanation))
+                    {
+                        output += " // " + codeExplanation + "\n";
+                    }
+                }
+                if(altCode is string && !String.IsNullOrEmpty(altCode))
+                {
+                    output += "* " + altCode + "\n";
+                }
+                if(document is string && !String.IsNullOrEmpty(document))
+                {
+                    output += "\n* Document: " + document + "\n";
+                }
+            }
+            output += "******************************  END OF DO-FILE  *****************************************************/";
+
+            label1.Label = "Processed " + rowCount + " rows";
+
+            if(!fileName.EndsWith(".do"))
+            {
+                fileName += ".do";
+            }
+            File.WriteAllText(folderPath + "\\" + fileName, output);
+        }
+    }
+}
